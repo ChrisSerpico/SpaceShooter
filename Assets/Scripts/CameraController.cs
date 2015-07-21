4a6559d7a13cbc5485182e43f1f699da7c00d2e8
Chris Serpico <christopherserpico2018@u.northwestern.edu>
2015-07-20 21:48:31 -0500

    Added some janky movement and a test area
    Movement will be revised in the coming days

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,13 +3,14 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+    // Turns the camera left and right when the player looks around 
+
+    // how fast the player rotates
+    public float rotationSpeed = 1.0f;
+
 	// Update is called once per frame
 	void Update () {
-	
+        float rotation = Input.GetAxis("Mouse X"); // get the amount of x-input
+        transform.Rotate(0, rotation * rotationSpeed, 0);
 	}
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    
+    // allows the character to be controlled via wasd/arrow keys
+
+    // multiplicative speed modifier
+    public float speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");  // Left and Right
+        float vertical = Input.GetAxis("Vertical");  // Forward and Back
+
+        // get the rigidbody of the player
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        rb.MovePosition(transform.position + (transform.forward * speed * vertical) + (transform.right * speed * horizontal));
+        //rb.MovePosition(transform.position + (transform.right * speed * horizontal));
+	}
+}
