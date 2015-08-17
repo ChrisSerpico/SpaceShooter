567ebebd9b1c752fcb2ef3ece2cd2649631fe75c
Chris Serpico <christopherserpico2018@u.northwestern.edu>
2015-08-17 16:04:55 -0500

    Added projectile collisions
    -Also messed with some numbers so the pistol feels more fun

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -10,4 +10,17 @@ public class Projectile : MonoBehaviour
 
     // how much damage this projectile should deal
     public int damage = 1;
+
+    // This code handles how the projectile should act if it collides with something
+    void OnTriggerEnter(Collider other)
+    {
+        // ignore the collision if the layers of the projectile and the collider are the same
+        // NOTE: This makes it really important to set layers correctly when making prefabs!
+        if (this.gameObject.layer == other.gameObject.layer) return;
+
+        //Debug.Log("Bullet Collision Detected!");
+        
+        // For now, just destroy the bullet. Once health is implemented, decrement health of "other" and then destroy the bullet
+        Destroy(this.gameObject);
+    }
 }
